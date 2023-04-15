9763bd181e9ca5b50cb6c5dbe28847fc13546b0e
Fabio Nocera <nofa1011@h-ka.de>
2023-04-15 23:58:40 +0200

    update rfid lib

--- include/RFIDLogin.h
+++ include/RFIDLogin.h
@@ -4,22 +4,24 @@
 #include <SPI.h>
 #include <MFRC522.h>
 
-class RFIDLogin {
-  public:
-    RFIDLogin(int rstPin, int ssPin);
-    void begin();
-    int scanCard();
-    int findUser(byte* uid);
-    int registerUser(byte* uid);
-    
-  private:
-    MFRC522 mfrc522;
-    MFRC522::MIFARE_Key key;
-    struct User {
-      byte uid[4];
-    };
-    User users[10];
-    int num_users;
+class RFIDLogin
+{
+public:
+  RFIDLogin(int rstPin, int ssPin);
+  void begin();
+  int scanCard();
+  int findUser(byte *uid);
+  int registerUser(byte *uid);
+
+private:
+  MFRC522 mfrc522;
+  MFRC522::MIFARE_Key key;
+  struct User
+  {
+    byte uid[4];
+  };
+  User users[10];
+  int num_users;
 };
 
 #endif
